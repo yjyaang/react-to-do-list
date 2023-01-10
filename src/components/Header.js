ef02464dba27bf42bc588f6918573a6a60b8390b
yjyaang <topbuty97@gmail.com>
2023-01-10 11:08:40 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    // if (window.location.pathname === '/') return null;
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Header
